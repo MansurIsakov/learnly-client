f0b7ca890d26f960fc3cbcf96325b7f2207ea864
MansurIsakov <mansurisakov.dev@gmail.com>
2022-10-25 18:49:59 +0500

    refactor: change user model && add images

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,25 +1,44 @@
-export class User {
-  constructor(
-    public user: {
-      _id: string;
-      _token: string;
-      firstName: string;
-      lastName: string;
-      email: string;
-      level: number;
-      course: string;
-      role: string;
-      emoji?: string;
-      dob?: string;
-      _createdAt?: string;
-      updatedAt?: string;
-    }
-  ) {}
+export interface User {
+  _id: string;
+  _token: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  level: number;
+  course: string;
+  role: string;
+  emoji?: string;
+  dob?: string;
+  _createdAt?: string;
+  updatedAt?: string;
+}
 
-  get token() {
-    if (!this.user._token) {
-      return null;
-    }
-    return this.user._token;
+export class UserModel implements User {
+  _id: string;
+  _token: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  level: number;
+  course: string;
+  role: string;
+  emoji?: string;
+  dob?: string;
+  _createdAt?: string;
+  updatedAt?: string;
+
+  constructor(source: User) {
+    this._id = source._id;
+    this._token = source._token;
+    this.firstName = source.firstName;
+    this.lastName = source.lastName;
+    this.email = source.email;
+    this.level = source.level;
+    this.course = source.course;
+    this.role = source.role;
+    this.emoji = source.emoji;
+    this.dob = source.dob;
+    this._createdAt = source._createdAt;
+    this.updatedAt = source.updatedAt;
   }
 }
