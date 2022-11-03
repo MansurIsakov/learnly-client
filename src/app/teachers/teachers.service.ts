8eb5ec627a66809c6f496f35341a6d679fd14bcd
MansurIsakov <mansurisakov.dev@gmail.com>
2022-11-03 20:06:31 +0500

    fix: minor changes

--- src/app/teachers/teachers.service.ts
+++ src/app/teachers/teachers.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, map, throwError } from 'rxjs';
+import { catchError, map, Subject, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ResponseData } from '../common/types/interfaces';
 import { ITeacher } from '../models/teacher.model';
 
 @Injectable({ providedIn: 'root' })
 export class TeachersService {
+  teachers: ITeacher[] = [];
+  teacher = new Subject<ITeacher>();
+
   constructor(private http: HttpClient) {}
 
   getAllTeachers() {
@@ -14,6 +17,9 @@ export class TeachersService {
       .get<ResponseData<ITeacher[]>>(environment.API_ENDPOINT + '/teachers')
       .pipe(
         map((resData) => {
+          this.teachers = resData.results;
+          console.log(this.teachers);
+
           return resData.results;
         }),
         catchError(this.handleError)
@@ -21,6 +27,9 @@ export class TeachersService {
   }
 
   getTeacher(id: string) {
+    // this.teacher.next(this.teachers.find((t) => t._id === id));
+    // console.log(this.teacher);
+
     return this.http
       .get<ResponseData<ITeacher>>(environment.API_ENDPOINT + '/teachers/' + id)
       .pipe(
