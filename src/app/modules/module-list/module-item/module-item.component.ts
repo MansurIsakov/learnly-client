c9d81817f6423e337356ccd1b1de0b50552d0a6a
MansurIsakov <mansurisakov.dev@gmail.com>
2022-11-07 22:34:25 +0500

    feat: finish ui

--- src/app/modules/module-list/module-item/module-item.component.ts
+++ src/app/modules/module-list/module-item/module-item.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-module-item',
   templateUrl: './module-item.component.html',
-  styleUrls: ['./module-item.component.scss']
+  styleUrls: ['./module-item.component.scss'],
 })
 export class ModuleItemComponent implements OnInit {
+  hexString = '0123456789abcdef';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  randomColor = () => {
+    let hexCode = '#';
+    for (let i = 0; i < 6; i++) {
+      hexCode +=
+        this.hexString[Math.floor(Math.random() * this.hexString.length)];
+    }
+    return hexCode;
+  };
+
+  generateGrad = () => {
+    let colorOne = this.randomColor();
+    let colorTwo = this.randomColor();
+    let angle = Math.floor(Math.random() * 360);
+
+    return `linear-gradient(${angle}deg, ${colorOne}, ${colorTwo})`;
+  };
 }
